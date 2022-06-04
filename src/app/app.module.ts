5f8713992448a46b2dbb87b5f21dfd281dfff7d5
myhomeworkfone <adzadurenne@gmail.com>
2022-06-04 02:00:42 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.modue';
 import { AppComponent } from './app.component';
+import { EnglishComponent } from './english/english.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    EnglishComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
